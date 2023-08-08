5992482bda5c2287cb4efabdec7280889b03f4d4
Jukka Matilainen <jukka.matilainen@euformatics.com>
2023-08-08 20:12:28 +0300

    Register a URLStreamHandler for 'cp:' URLs
    Introduce a URLStreamHandler for dereferencing 'cp:/' URLs, allowing to
    retrieve the contents from classpath resources.  This implementation
    supports an optional leading slash in the URL paths, which allows the
    URLs to be treated as hiererchical, supporting the use of relative URL
    references.

---
+++ src/main/java/com/stackoverflow/q76848364/ClasspathURLStreamHandlerProvider.java
@@ -0,0 +1,38 @@
+package com.stackoverflow.q76848364;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+import java.net.spi.URLStreamHandlerProvider;
+
+/**
+ * URL stream handler for "cp:/" URLs for accessing resources in the classpath.
+ * Supports a leading slash in the the path so that the scheme is treated as a
+ * hierarchical scheme for resolving relative URL references.
+ * 
+ * <p>
+ * Register this provider by putting the fully qualified name of this class in
+ * the configuration file
+ * META-INF/services/java.net.spi.URLStreamHandlerProvider.
+ */
+public class ClasspathURLStreamHandlerProvider extends URLStreamHandlerProvider {
+
+    private static final String PROTOCOL = "cp";
+
+    @Override
+    public URLStreamHandler createURLStreamHandler(String protocol) {
+        if (PROTOCOL.equals(protocol)) {
+            return new URLStreamHandler() {
+                @Override
+                protected URLConnection openConnection(URL url) throws IOException {
+                    String urlPath = url.getPath();
+                    String resourcePath = urlPath.startsWith("/") ? urlPath.substring(1) : urlPath;
+                    return ClassLoader.getSystemClassLoader().getResource(resourcePath).openConnection();
+                }
+            };
+        }
+        return null;
+    }
+
+}
